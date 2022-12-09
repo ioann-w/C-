449efc76b0b4f669f4a39b91ef02520f67c69834
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-09 23:08:33 +0300

    3 ex its ok

--- hw/cshw3/3_ex/Program.cs
+++ hw/cshw3/3_ex/Program.cs
@@ -4,3 +4,32 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+
+
+
+bool isCorrect = false;
+int num = 0;
+
+while (!isCorrect)
+{
+    Console.Write("Введите число: ");
+    string strnum = Console.ReadLine() ?? "";
+
+    if (int.TryParse(strnum, out num))
+    {
+        isCorrect = true;
+    }
+    else
+    {
+        Console.WriteLine("Некорректное число");
+        break;
+    }
+
+    
+    Console.Write($"Таблица кубов чисел от 1 до {num}: ");
+    
+    for (int i = 1; i <= num; i++)
+        Console.Write($"{Math.Pow(i, 3)}, ");
+
+}
+
