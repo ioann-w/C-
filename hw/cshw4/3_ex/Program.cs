40ffdebe573f0c323656dfe502cc833867e60ba5
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-11 15:51:40 +0300

    3 ex its ok

--- hw/cshw4/3_ex/Program.cs
+++ hw/cshw4/3_ex/Program.cs
@@ -5,3 +5,20 @@
 
 6, 1, 33 -> [6, 1, 33]
 */
+
+
+int[] eightnum = new int[8];
+
+Random randomnum = new Random();
+
+Console.Clear();
+Console.WriteLine("Элементы массива: ");
+Console.WriteLine("");
+
+for (int i = 0; i < eightnum.Length; i++)
+{
+    eightnum[i] = randomnum.Next();
+
+    Console.WriteLine(eightnum[i]);
+    Console.WriteLine("");
+}
