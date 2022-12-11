3baa47c13cac0435f147ad2d185d9cd57b61fbed
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-11 14:54:00 +0300

    1 ex its ok

--- hw/cshw4/1_ex/Program.cs
+++ hw/cshw4/1_ex/Program.cs
@@ -5,3 +5,43 @@
 
 2, 4 -> 16
 */
+
+
+/* Не совсем понял условия задачи, зачем цикл? 
+В смысле, нужно было сделать чтобы программа бесконечно просила вводить новые
+числа после того как завершила первую итерацию ? */
+
+bool isCorrect = false;
+int num1 = 0;
+int num2 = 0;
+
+while (!isCorrect)
+{
+    Console.Write("Введите первое число: ");
+    string strnum1 = Console.ReadLine() ?? "";
+
+    Console.WriteLine("");
+
+    if (int.TryParse(strnum1, out num1))
+    {
+        isCorrect = true;
+    }
+    else
+    {
+        Console.WriteLine("Некорректное число");
+    }
+    
+    Console.Write("Введите второе число: ");
+    string strnum2 = Console.ReadLine() ?? ""; 
+      
+    if (int.TryParse(strnum2, out num2))
+    {
+        isCorrect = true;
+    }
+    else
+    {
+        Console.WriteLine("Некорректное число");
+    }
+        Console.Clear();
+        Console.WriteLine($"{num1} в степени {num2} = {Math.Pow(num1, num2)}");
+}
