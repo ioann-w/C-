fd0afb13501526bbb20d7390539424fc82746693
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-11 15:31:55 +0300

    2 ex its ok

--- hw/cshw4/2_ex/Program.cs
+++ hw/cshw4/2_ex/Program.cs
@@ -7,3 +7,27 @@
 
 9012 -> 12
 */
+
+bool isCorrect = false;
+int num = 0;
+
+while (!isCorrect)
+{
+    Console.Write("Введите число: ");
+    string value = Console.ReadLine () ?? "";
+
+    if (int.TryParse(value, out num))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число");
+            break;
+        }
+
+    int sum = 0;
+    for (int i = 0; i < value.Length; i++)
+        sum = sum + int.Parse(value[i].ToString());
+    Console.WriteLine($"Сумма всех цифр числа {num}, равна {sum}");
+}
