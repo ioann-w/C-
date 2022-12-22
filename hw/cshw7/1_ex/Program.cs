8cf876ec5d9942522dcf512512487f484dfecd96
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-22 20:32:24 +0300

    1 ex its ok

---
+++ hw/cshw7/1_ex/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+*/
+
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+var m = Convert.ToInt32(Console.ReadLine()?? "");
+
+Console.Write("Введите количество столбцов в массиве: ");
+var n = Convert.ToInt32(Console.ReadLine()?? "");
+
+double[,] arr = new double[m, n];
+Random rnd = new ();
+
+for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rnd.Next(-10, 100) + rnd.NextDouble();
+        }
+    }
+
+Console.WriteLine("");
+Console.WriteLine("Массив: ");
+Console.WriteLine("");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+        Console.WriteLine($"| {arr[i, j]}");
+    Console.WriteLine();
+}
+
+
