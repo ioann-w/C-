29d492c9d19489fc49fdcff8d8cf6052a761f204
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-28 21:02:44 +0300

    2 ex its ok

--- hw/cshw8/2_ex/Program.cs
+++ hw/cshw8/2_ex/Program.cs
@@ -48,3 +48,64 @@ PrintArray(arr);
 Console.WriteLine($"Минимальную сумму имеет строка номер: {x}");
 */
 
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+var a = Convert.ToInt32(Console.ReadLine()?? "");
+
+Console.Write("Введите количество столбцов в массиве: ");
+var b = Convert.ToInt32(Console.ReadLine()?? "");
+
+int[,] arr = new int[a, b];
+Random rnd = new ();
+
+for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            arr[i, j] = rnd.Next(0, 10);
+        }
+    }
+
+void PrintArr (int [,] arr)
+{
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+            Console.Write($" {arr[i, j]}   ");
+        Console.WriteLine();
+    }
+}
+
+int GetSum(int [,] arr)  
+{  
+    int minSum = 0;
+    int minIndex = 0;
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        minSum += arr[0, i];
+    }
+    Console.WriteLine($"Строка {1}, сумма элементов {minSum}");
+
+    for (int i = 1; i < arr.GetLength(0); i++) 
+    {   
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++) 
+        {
+            sum += arr[i, j];
+        } 
+        Console.WriteLine($"Строка {i + 1}, сумма элементов {sum}");
+        if(sum < minSum)
+        {
+            minSum = sum;
+            minIndex = i;
+        }
+    }
+            
+    return minIndex + 1;
+}
+
+Console.WriteLine("Массив сгенерирован: ");
+PrintArr(arr);
+int minSum = GetSum(arr);
+Console.WriteLine($"Строка с минимальной суммой: {minSum}");
