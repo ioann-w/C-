30515b43efc09e727ea396908a48ee71c0ee9416
Ivan Shundeev <i.shundeev@gmail.com>
2022-12-29 21:36:54 +0300

    4 ex its ok

--- hw/cshw8/4_ex/Program.cs
+++ hw/cshw8/4_ex/Program.cs
@@ -7,44 +7,31 @@
 26(1,0,1) 55(1,1,1)
 */
 
-/*
-int x = 2;
-int y = 2;
-int z = 2;
+Console.Clear();
 
-int[,,] arr = new int[x, y, z];
-Random rand = new();
+int [,,] GetArr(int rows, int columns, int depth)  
+{ 
+    Random rnd = new Random(); 
+    int [,,] arr = new int [rows,columns, depth]; 
+    for (int i = 0; i < arr.GetLength(0); i++) 
+        for (int j = 0; j < arr.GetLength(1); j++) 
+            for (int k = 0; k < arr.GetLength(2); k++)
+                arr[i,j,k] = rnd.Next(0, 100); 
+    return arr;    
+} 
 
-bool NumExists(int num)
-{
-    for (int i = 0; i < x; i++)
-        for (int j = 0; j < y; j++)
-            for (int t = 0; t < z; t++)
-                if (arr[i, j, t] == num) return true;
-    return false;
-}
-
-for (int i = 0; i < x; i++)
-    for (int j = 0; j < y; j++)
-        for (int t = 0; t < z; t++)
+void PrintArray (int [,,] arr) 
+{ 
+    for (int i = 0; i < arr.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < arr.GetLength(1); j++) 
         {
-            int num;
-            do
-            {
-                num = rand.Next(10, 99);
-            } while (NumExists(num));
-            arr[i, j, t] = num;
+            for (int k = 0; k < arr.GetLength(2); k++)
+                Console.Write($"{arr[i,j,k]}({i},{j},{k})\t"); 
+            Console.WriteLine();     
         }
-
-for (int i = 0; i < x; i++)
-{
-    for (int j = 0; j < y; j++)
-    {
-        for (int t = 0; t < z; t++)
-            Console.Write(String.Format("{0,4:0.#}", arr[i, j, t]) + $" ({i}, {j}, {t})");
-        Console.WriteLine();
-    }
+    }  
 }
-
-*/
-
+ 
+int [,,] okArray = GetArr(3, 3, 3); 
+PrintArray(okArray);
